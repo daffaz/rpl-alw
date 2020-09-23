9018cd64a05f14f8419d10a02a5061998df71c29
daffaz <67418594+daffaz@users.noreply.github.com>
2020-09-24 00:16:39 +0700

    Update hasil.php

--- hasil.php
+++ hasil.php
@@ -56,7 +56,7 @@ include 'function.php';
       width: 20%;
     }
   </style>
-  <title>Hello, world!</title>
+  <title>Hasil</title>
 </head>
 
 <body style="background-color:#E5E5E5; color: white;">
